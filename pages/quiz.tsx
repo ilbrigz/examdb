2b67e2d8541036bdf26ef6d180ba26fcbac293f1
ilbrigz <ilogirb23@yahoo.com>
2022-06-28 19:47:09 +0800

    .

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PaginatedQuestions from '../components/PaginatedQuestions';
 
 type Props = {};
 
 const quiz = (props: Props) => {
+  useEffect(() => {
+    window.onbeforeunload = function () {
+      return confirm('Are you sure you want to reload? Progress will be lost.');
+    };
+  }, []);
   return (
     <>
       <PaginatedQuestions />
